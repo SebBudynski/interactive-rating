f6389e33b11b962c15b297a690d3c2b1e2febb7a
Sebastian Budyński <Sebbudynski@gmail.com>
2024-01-15 14:59:49 +0100

    cleaner code, disebled submit before chose rate

--- app.js
+++ app.js
@@ -2,18 +2,20 @@
 
 const submit = document.querySelector(".submit");
 const rates = document.querySelectorAll(".rate");
-const rated = document.getElementById("rated")
+const rated = document.getElementById("rated");
 
+submit.disabled = true;
 
 // switching to thank you panel//
 submit.addEventListener("click", () => {
-  document.querySelector(".question").classList.toggle("hidden");
-  document.querySelector(".thank-you").classList.toggle("hidden");
+  document.querySelector(".question").classList.add("hidden");
+  document.querySelector(".thank-you").classList.remove("hidden");
 });
 
-//displaing choosen rate in thank-you panel//
+//displaing rate in thank-you panel//
 rates.forEach((rate) => {
   rate.addEventListener("click", () => {
+    submit.disabled = false;
     rated.innerHTML = rate.innerHTML;
-  })
-})
+  });
+});
